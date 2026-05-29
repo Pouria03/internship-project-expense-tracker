190c53c6a6751b02066b3c5b049d42ed28d44be4
Pouria03 <shaiganidev@gmail.com>
2026-05-29 17:52:53 +0330

    add user model (table)

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    Custom User model inheriting from AbstractUser.
+    Ready for future extensions (e.g., avatar, phone, etc.).
+    """
+
+    class Meta:
+        db_table = 'users'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return self.username
