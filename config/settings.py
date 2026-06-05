e7e45d78b2d6a6129b351fb569743594294e3b03
Pouria03 <shaiganidev@gmail.com>
2026-06-05 20:23:19 +0330

    Configure auth URLs and login redirect settings

--- config/settings.py
+++ config/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +124,6 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Auth user model (custom user model) 
 AUTH_USER_MODEL = 'accounts.User'
+
+LOGIN_URL = 'accounts:login'
+LOGIN_REDIRECT_URL = 'home'
