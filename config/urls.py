f61b2d1737a5478eb77591337bd35486b13236db
Pouria03 <shaiganidev@gmail.com>
2026-06-19 20:19:06 +0330

    Fix static files in production

--- config/urls.py
+++ config/urls.py
@@ -11,3 +11,8 @@ urlpatterns = [
     path('export/', include('apps.expenses.urls.excel_urls')),
     path('', home_view, name='home'),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
